b9492e35709cc84dc14e7b2420d71858a20d17d7
nikita7031 <79699356+nikita7031@users.noreply.github.com>
2021-05-23 15:04:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let play=0;
 
 document.querySelector('body').addEventListener('click', function() {
     if(play==0) {
-        var audio = new Audio('https://pmp3.ru/uploads/music/russian/2020/10/unnv_kateeechi_-_bez_datyi.mp3');
+        var audio = new Audio('https://rux.kilosound.org/get/music/20201203/upmusicme_Alexa_the_Fiends_-_JEto_novyjj_Bravl_stars_71832665.mp3');
     audio.play();
     document.querySelector('.text').style.visibility='hidden';
     play=1
